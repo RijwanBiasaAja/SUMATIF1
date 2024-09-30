fdf5a94793e739b960d7a8d27eadc8ad979b69c6
RijwanBiasaAja <aprijal441@gmail.com>
2024-09-30 09:13:48 +0700

    update

---
+++ main.js
@@ -0,0 +1,60 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
+import { 
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDies7HBA_N8xfk_8DJlM-EFbi_GTXDJpI",
+  authDomain: "insan-cemerlang-996a1.firebaseapp.com",
+  projectId: "insan-cemerlang-996a1",
+  storageBucket: "insan-cemerlang-996a1.appspot.com",
+  messagingSenderId: "137591161633",
+  appId: "1:137591161633:web:e89f54d3cf2a29d9fdb460",
+  measurementId: "G-B5KFGBXLMV"
+};
+
+// Inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
+
+export async function ambilDaftarPelanggan() {
+  const refDokumen = collection(basisdata, "pelanggan");
+  const kueri = query(refDokumen, orderBy("nama pelanggan"));
+  const cuplikankueri = await getDocs(kueri);
+  
+  let hasilkueri = [];
+  cuplikankueri.forEach((dokumen) => {
+    hasilkueri.push({
+      id: dokumen.id,
+      nama: dokumen.data().nama,
+      tlpon: dokumen.data().tlpon
+    })
+  })
+  
+  return hasilkueri;
+}
+
+export async function tambahDataPelanggan(nama, tlpon) {
+  try {
+    // menyimpan data ke firebase
+    const refDokumen = await addDoc(collection(basisdata, "pelanggan"), {
+      nama: nama,
+      tlpon: tlpon
+    })
+    
+    // menampilkan pesan berhasil
+    console.log("berhasil menyimpan data pelanggan")
+  } catch (error) {
+  // menampilkan pesan gagal
+  console.console.log("gagal menyimpan data pelanggan")
+  }
+}
